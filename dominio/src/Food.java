5d1d78a5a47ce925320547c31a66dbfb79bb0176
Carlos Orduz <carlos.orduz@mail.escuelaing.edu.co>
2021-04-27 18:41:07 -0500

    Food and Snake clasessadded

---
+++ dominio/src/Food.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Food {
+    private int positionX;
+    private int positionY;
+    
+    public void createFood(){
+
+    }
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+}
